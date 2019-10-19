7785c86aa1ae3f229aafae440ebc0665cd1f2142
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-10-19 12:05:39 +0400

    Update script.js

--- L301/script.js
+++ L301/script.js
@@ -1,8 +1,13 @@
 
-let str = 'საქართველოში გამოაქვეყნეს რეკლამა, რომელიც შეიქმნა ვირუსი სგან, ეს არის ცუდი მარკეტინგი ს ბრალი';
+let str = 'საქართველოში გამოაქვეყნეს ,მარკეტინგი რომელიც შეიქმნა ვირუსი  სგან, ეს არის ცუდი ს ბრალი';
+document.querySelector('p').textContent = str;
+let myArr = ['რეკლამა','ვირუსი','მარკეტინგი']
+                for (var i = 0; i < myArr.length; i++) {
+                    if(str.indexOf(myArr[i]) != -1) {
 
-document.write(str)
-        if (str.indexOf('რეკლამა') || str.indexOf('ვირუსი') || str.indexOf('მარკეტინგი')) {
-            alert('თქვენ იყენებთ აკრძალულ სიტყვებს');
-        }
+                        alert('თქვენ იყენებთ აკრძალულ სიტყვებს: ' +myArr[i]);
+                    } else {
+                        continue;
+                }
+            }
         
